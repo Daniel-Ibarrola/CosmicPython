db6c218cb7e4b5003eb58592c8d9562e478a778e
Daniel-Ibarrola <dis96051@gmail.com>
2022-12-25 09:46:44 -0600

    Setup tests to use postgre db and flask

--- flask_app.py
+++ flask_app.py
@@ -8,4 +8,19 @@ import orm
 import repository
 
 
+orm.start_mappers()
+get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
+
+
+@app.route("/allocate", methods=["POST"])
+def allocate_endpoint():
+    session = get_session()
+    batches = repository.SqlAlchemyRepository(session).list()
+    line = model.OrderLine(
+        request.json["orderid"], request.json["sku"], request.json["qty"],
+    )
+
+    batchref = model.allocate(line, batches)
+
+    return {"batchref": batchref}, 201
--- test_api.py
+++ test_api.py
@@ -1,18 +1,24 @@
+import uuid
 import pytest
-import config
 import requests
 
-
-def random_sku():
-    pass
+import config
 
 
-def random_batchref():
-    pass
+def random_suffix():
+    return uuid.uuid4().hex[:6]
 
 
-def random_orderid():
-    pass
+def random_sku(name=""):
+    return f"sku-{name}-{random_suffix()}"
+
+
+def random_batchref(name=""):
+    return f"batch-{name}-{random_suffix()}"
+
+
+def random_orderid(name=""):
+    return f"order-{name}-{random_suffix()}"
 
 
 @pytest.mark.usefixtures("restart_api")
